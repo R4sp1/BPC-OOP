c9d988bcb6b48e3789e65471a4829fab0eb082f5
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2022-02-14 17:00:01 +0100

    Hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        // Vstupní bod programu
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // čekání na stisk klávesy
+            Console.ReadLine();
+        }
+    }
+}
