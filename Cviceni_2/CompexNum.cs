89065a39843d4c4bf2235104d0b31f03d518076e
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2022-02-15 09:16:56 +0100

    Pridana trida ComplexNum

---
+++ Cviceni_2/CompexNum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Cviceni_2
+{
+    public class CompexNum
+    {
+        public double Real;
+        public double Imag;
+        
+        public CompexNum(double real, double imag) //Konstruktor kompexniho cisla
+        {
+            Real = real;
+            Imag = imag;
+        }
+
+        public static CompexNum operator +(CompexNum a, CompexNum b) //Scitani
+        {
+            return new CompexNum(a.Real + b.Real, a.Imag + b.Imag);
+        }
+
+        public static CompexNum operator -(CompexNum a, CompexNum b) //Odcitani
+        {
+            return new CompexNum(a.Real - b.Real, a.Imag - b.Imag);
+        }
+
+        public static CompexNum operator *(CompexNum a, CompexNum b) //Nasobeni
+        {
+            return new CompexNum((a.Real * b.Real) - (a.Imag * b.Imag), (a.Real * b.Imag) + (a.Imag * b.Real));
+        }
+
+        public static CompexNum operator /(CompexNum a, CompexNum b) //Deleni
+        {
+            return new CompexNum((a.Real * b.Real + a.Imag * b.Imag) / (b.Imag * b.Imag + b.Real * b.Real),
+                                 (a.Imag * b.Real - a.Real * b.Imag) / (b.Imag * b.Imag + b.Real * b.Real));
+        }
+
+        public static bool operator ==(CompexNum a, CompexNum b)
+        {
+            return a.Real == b.Real && a.Imag == b.Imag;
+        }
+
+        public static bool operator !=(CompexNum a, CompexNum b) //Pro operator == musim definovat take !=
+        {
+            return !(a == b); //Uz mam definovano v opeeratoru ==
+        }
+
+        public override string ToString() //Vypis do retezce
+        {
+            if (Imag < 0)
+            {
+                return string.Format("{0}-{1}j", Real, -Imag);
+            }
+            else
+            {
+                return string.Format("{0}+{1}j", Real, Imag);
+            }
+        }
+
+    }
+}
--- Cviceni_2/Program.cs
+++ Cviceni_2/Program.cs
@@ -7,8 +7,9 @@ namespace Cviceni_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-
+            CompexNum x = new CompexNum(1.2, 0.5); // vytvoření instance třídy
+            CompexNum y = new CompexNum(2.0, -1.5); // vytvoření instance třídy
+            Console.WriteLine(x + y);
             Console.ReadLine();
         }
     }
