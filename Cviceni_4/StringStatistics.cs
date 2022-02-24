d456ae9e15578ae15f47077bc4d870328bf25289
R4sp1 <57303554+R4sp1@users.noreply.github.com>
2022-02-24 16:48:34 +0100

    Updated StringStatistic.cs

--- Cviceni_4/StringStatistics.cs
+++ Cviceni_4/StringStatistics.cs
@@ -17,5 +17,49 @@ namespace Cviceni_4{
             var count = text.Split('\n').Length;
             return count;
         }
+
+        public int SentenceCount(){
+            var countPeriods = text.Split('.').Length;
+            var countExclamationMarks = text.Split('!').Length;
+            var countQuestionMarks = text.Split('?').Length;
+            return countPeriods + countExclamationMarks + countQuestionMarks;
+        }
+
+        public List<string> LongestWord(){
+            var justText = text.Replace(".", "").Replace(",", "").Replace("?", "").Replace("!", "");
+            string[] wordsString = justText.Split(' ');
+
+            var orderedString = wordsString.OrderBy(x => x.Length).ToList<string>();
+            orderedString.Reverse();
+
+            var finalList = new List<string>();
+            var len = 0;
+            foreach (var word in orderedString) {
+                if (word.Length >= len) {
+                    len = word.Length;
+                    finalList.Add(word);
+                }
+            }
+
+            return finalList;
+        }
+
+        public List<string> ShortestWord(){
+            var justText = text.Replace(".", "").Replace(",", "").Replace("?", "").Replace("!", "");
+            string[] wordsString = justText.Split(' ');
+
+            var orderedString = wordsString.OrderBy(x => x.Length).ToList<string>();
+
+            var finalList = new List<string>();
+            var len = 1000;
+            foreach (var word in orderedString) {
+                if (word.Length <= len) {
+                    len = word.Length;
+                    finalList.Add(word);
+                }
+            }
+
+            return finalList;
+        }
     }
 }
